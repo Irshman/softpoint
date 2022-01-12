b98ffe4fb5b19d814211915a2d70141821e72825
Irshman <76690755+Irshman@users.noreply.github.com>
2022-01-12 22:51:57 +0200

    add project

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+    host: "", //  Адресс FTP сервера
+    user: "", // Имя пользователя
+    password: "", // Пароль
+    parallel: 5 // Кол-во одновремених потоков
+}
---
+++ gulp/config/plugins.js
@@ -0,0 +1,16 @@
+import replace from "gulp-replace"; // Поиск и замена
+import plumber from "gulp-plumber"; // Обработка ошибок
+import notify from "gulp-notify"; // Сообщения (подсказка)
+import browsersync from "browser-sync"; // Local server
+import newer from 'gulp-newer'; // Проверка обновлений 
+import ifPlugin from 'gulp-if'; // Условное ветление 
+
+// Експортируем обєкт 
+export const plugins = {
+    replace: replace,
+    plumber: plumber,
+    notify: notify,
+    browsersync: browsersync,
+    newer: newer,
+    if: ifPlugin
+}
